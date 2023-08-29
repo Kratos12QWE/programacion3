0acf994acf6033760aaea804259824721226a406
Kratos12QWE <cbbe.carlosalejandro.morales.ni@unifranz.edu.bo>
2023-08-29 18:00:43 -0400

    version 2023-08-29_01

--- ahorcado.py
+++
@@ -1,6 +0,0 @@
-def jugar_ahorcado():  
-    print('========================================')
-    print('Bienvenido al Juego del ahorcado')
-    print('========================================')
-
-    print('fin del juego')
