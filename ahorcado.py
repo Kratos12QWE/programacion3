f307f2258b37fc7c3c15df5e2810585e108861e6
Kratos12QWE <cbbe.carlosalejandro.morales.ni@unifranz.edu.bo>
2023-09-19 17:05:15 -0400

    version 2023-09-19_01

--- ahorcado.py
+++ ahorcado.py
@@ -1,29 +1,43 @@
+import random
+
 def jugar():
 
     print('================================')
     print('Bienvenido al Juego del Ahorcado')
     print('================================')
+       
+    archivo=open('palabras.txt','r')
+    palabras=[]
+    for linea in archivo:
+        linea=linea.strip()
+        palabras.append(linea)
+        
+        
+    archivo.close()
+    numero=random.randrange(0,len(palabras))
     
-     
-    palabra_secreta = 'mandarina'
-    letras_acertadas=['_','_','_','_','_','_','_','_','_']
+    palabra_secreta = palabras[numero].lower()
+    letras_acertadas =['_' for elemento in palabra_secreta]
     
     ahorcado = False
     acerto = False
     
+    print(letras_acertadas)
     while ( not ahorcado and not acerto ):
-        entrada = input ("ingrese una letra...")
-        entrada = entrada.strip()
-        entrada = entrada.lower()
+        entrada=input ("ingrese una letra...")
+        entrada=entrada.strip()
+        entrada=entrada.lower()
         indice = 0
         for letra in palabra_secreta:
-            if(entrada == letra):
-                letras_acertadas[indice]=letra
-            
-            indice = indice +1
+                if(entrada==letra):
+                    letras_acertadas[indice] = letra
+                
+                indice = indice + 1     
         print(letras_acertadas)
-
-    print("Fin del Juego")
+        print('jugando...')
+    print('fin del juego')
+     
     
 if(__name__ == "__main__"):
-   jugar()
+
+    jugar()
